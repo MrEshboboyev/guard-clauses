d9cf1195a23a2649a423467273d6e4f35acb0075
mreshboboyev <mreshboboyev@gmail.com>
2025-01-22 17:10:40 +0500

    Custom Guard Clauses

---
+++ GuardClauses/Ensure.cs
@@ -0,0 +1,26 @@
+﻿namespace GuardClauses;
+
+public static class Ensure
+{
+    public static void NotNull<T>(T? value, string? paramName = null)
+    {
+        if (value is null) throw new ArgumentNullException(
+            "The value cannot be null",
+            paramName);
+    }
+    
+    public static void NotNullOrEmpty(string? value, string? paramName = null)
+    {
+        if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(
+            "The string cannot be null nor empty", 
+            paramName);
+    }
+    
+    public static void NotNullOrEmptyList<T>(List<T>? list, string? paramName = null)
+    {
+        if (list is null || list.Count == 0)
+        {
+            throw new ArgumentException("The list should contain at least 1 item.", paramName);
+        }
+    }
+}
--- GuardClauses/Program.cs
+++ GuardClauses/Program.cs
@@ -12,10 +12,14 @@ Console.WriteLine("Hello, World!");
 
 static void ProcessOrder(Order order)
 {
-    ArgumentNullException.ThrowIfNull(order);
-    ArgumentException.ThrowIfNullOrEmpty(order.CustomerName);
-    ArgumentNullException.ThrowIfNull(order.Products);
-    ArgumentOutOfRangeException.ThrowIfZero(order.Products.Count);
+    // ArgumentNullException.ThrowIfNull(order);
+    Ensure.NotNull(order, nameof(order));
+    
+    // ArgumentException.ThrowIfNullOrEmpty(order.CustomerName);
+    Ensure.NotNullOrEmpty(order.CustomerName, nameof(order.CustomerName));
+    
+    // ArgumentNullException.ThrowIfNull(order.Products);
+    Ensure.NotNullOrEmptyList(order.Products, nameof(order.Products));
 
     Console.WriteLine($"User {order.CustomerName} has ordered:");
 
