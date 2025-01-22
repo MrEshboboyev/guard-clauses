36a44f2da49c1c69e3e3eee6406b1d603a7832c0
mreshboboyev <mreshboboyev@gmail.com>
2025-01-22 17:03:01 +0500

    Better way : .NET Built-in Guard Clauses

--- GuardClauses/Program.cs
+++ GuardClauses/Program.cs
@@ -12,26 +12,15 @@ Console.WriteLine("Hello, World!");
 
 static void ProcessOrder(Order order)
 {
-    if (order is null)
-    {
-        throw new ArgumentException("The order is required.", nameof(order));
-    }
-
-    if (string.IsNullOrEmpty(order.CustomerName))
-    {
-        throw new ArgumentException("The Customer Name is required.", nameof(order.CustomerName));
-    }
+    ArgumentNullException.ThrowIfNull(order);
+    ArgumentException.ThrowIfNullOrEmpty(order.CustomerName);
+    ArgumentNullException.ThrowIfNull(order.Products);
+    ArgumentOutOfRangeException.ThrowIfZero(order.Products.Count);
 
     Console.WriteLine($"User {order.CustomerName} has ordered:");
-
-    if (order.Products is null || order.Products.Count == 0)
-    {
-        throw new ArgumentException("The order should contain at least 1 product", nameof(order.Products));
-    }
 
     foreach (var product in order.Products)
     {
         Console.WriteLine($"{product}");
     }
 }
-
