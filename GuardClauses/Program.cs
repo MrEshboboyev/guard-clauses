2fb9d40b7041e657a6ad31584f0a7b1d1586c263
mreshboboyev <mreshboboyev@gmail.com>
2025-01-22 16:59:34 +0500

    Refactoring with Guard Clauses

--- GuardClauses/Program.cs
+++ GuardClauses/Program.cs
@@ -12,31 +12,26 @@ Console.WriteLine("Hello, World!");
 
 static void ProcessOrder(Order order)
 {
-    if (order is not null)
+    if (order is null)
     {
-        if (string.IsNullOrEmpty(order.CustomerName))
-        {
-            Console.WriteLine($"User {order.CustomerName} ordered:");
-        }
-        else
-        {
-            throw new ArgumentException("The Customer Name is required.", nameof(order.CustomerName));
-        }
-
-        if (order.Products is not null && order.Products.Count > 0)
-        {
-            foreach (var product in order.Products)
-            {
-                Console.WriteLine($"{product}");
-            }
-        }
-        else
-        {
-            throw new ArgumentException("The order should contain at least 1 product", nameof(order.Products));
-        }
+        throw new ArgumentException("The order is required.", nameof(order));
     }
-    else
+
+    if (string.IsNullOrEmpty(order.CustomerName))
     {
-        throw new ArgumentException("The order is required", nameof(order));
+        throw new ArgumentException("The Customer Name is required.", nameof(order.CustomerName));
+    }
+
+    Console.WriteLine($"User {order.CustomerName} has ordered:");
+
+    if (order.Products is null || order.Products.Count == 0)
+    {
+        throw new ArgumentException("The order should contain at least 1 product", nameof(order.Products));
+    }
+
+    foreach (var product in order.Products)
+    {
+        Console.WriteLine($"{product}");
     }
 }
+
