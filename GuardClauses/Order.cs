e72754d860ebdffbfa4cf710e6ed25e312fe5ddb
mreshboboyev <mreshboboyev@gmail.com>
2025-01-22 16:54:12 +0500

    Simple code example from Junior ))

---
+++ GuardClauses/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace GuardClauses;
+
+public class Order
+{
+    public string CustomerName { get; set; } = string.Empty;
+    public List<string> Products { get; set; } = [];
+}
---
+++ GuardClauses/Program.cs
@@ -0,0 +1,42 @@
+﻿using GuardClauses;
+
+Order order = new()
+{
+    CustomerName = "Stefan",
+    Products = ["apple", "juice", "bread"]
+};
+
+ProcessOrder(order);
+
+Console.WriteLine("Hello, World!");
+
+static void ProcessOrder(Order order)
+{
+    if (order is not null)
+    {
+        if (string.IsNullOrEmpty(order.CustomerName))
+        {
+            Console.WriteLine($"User {order.CustomerName} ordered:");
+        }
+        else
+        {
+            throw new ArgumentException("The Customer Name is required.", nameof(order.CustomerName));
+        }
+
+        if (order.Products is not null && order.Products.Count > 0)
+        {
+            foreach (var product in order.Products)
+            {
+                Console.WriteLine($"{product}");
+            }
+        }
+        else
+        {
+            throw new ArgumentException("The order should contain at least 1 product", nameof(order.Products));
+        }
+    }
+    else
+    {
+        throw new ArgumentException("The order is required", nameof(order));
+    }
+}
